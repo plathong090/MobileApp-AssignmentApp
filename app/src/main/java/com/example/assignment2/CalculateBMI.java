3609f62aff9c0252beadf1fdb40ac41b7d551ad3
plathong090 <prangthong2500947@gmail.com>
2025-01-06 00:16:02 +0700

    finish calculate grade

--- app/src/main/java/com/example/assignment2/CalculateBMI.java
+++ app/src/main/java/com/example/assignment2/CalculateBMI.java
@@ -1,6 +1,9 @@
 package com.example.assignment2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CalculateBMI extends AppCompatActivity {
+public class CalculateBMI extends AppCompatActivity implements View.OnClickListener {
 
+    private Button BackBtn;
+    //@SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.calculate_bmi);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        BackBtn = findViewById(R.id.BackBMIBtn);
+        BackBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        finish();
+
     }
 }
--- app/src/main/java/com/example/assignment2/CalculateGrade.java
+++ app/src/main/java/com/example/assignment2/CalculateGrade.java
@@ -1,6 +1,10 @@
 package com.example.assignment2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,90 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CalculateGrade extends AppCompatActivity {
+public class CalculateGrade extends AppCompatActivity implements View.OnClickListener {
+
+    private Button BackBtn , CalGradeBtn;
+    private EditText InputScore , OutputGrade , ResultGrade ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.calculate_grade);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        InputScore = findViewById(R.id.InputScore);
+        InputScore.setHint("ใส่ตัวเลขทศนิยม");
+
+        OutputGrade = findViewById(R.id.OutputGrade);
+        ResultGrade = findViewById(R.id.ResultGrade);
+
+        //ปุ่มย้อนกลับหน้าหลัก
+        BackBtn = findViewById(R.id.BackGradeBtn);
+        BackBtn.setOnClickListener(this);
+
+        //ปุ่มคำนวณ
+        CalGradeBtn = findViewById(R.id.CalGradeBtn);
+        CalGradeBtn.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.CalGradeBtn) {
+            calculate();
+
+        } else if (v.getId() == R.id.BackGradeBtn) {
+            finish();
+        }
+    }
+
+    private void calculate() {
+        String input = InputScore.getText().toString();
+
+        //check การกรอก input
+        if (!input.isEmpty()) {
+            try {
+                int score = Integer.parseInt(input); //ตรวจสอบว่า score เป็นตัวเลขหรือไม่
+                String grade = "";
+
+                if (score >= 101) {
+                    Toast.makeText(this, "คะแนน " + score + " ไม่ถูกต้อง", Toast.LENGTH_SHORT).show();
+                } else if (score >= 80) {
+                    grade = "A";
+                } else if (score >= 75) {
+                    grade = "B+";
+                } else if (score >= 70) {
+                    grade = "B";
+                } else if (score >= 65) {
+                    grade = "C+";
+                } else if (score >= 60) {
+                    grade = "C";
+                } else if (score >= 55) {
+                    grade = "D+";
+                } else if (score >= 50) {
+                    grade = "D";
+                } else {
+                    grade = "F";
+                }
+
+                //แสดงผล
+                OutputGrade.setText(String.valueOf(score));
+                ResultGrade.setText(grade);
+
+                //ล้างช่อง InputScore และตั้งค่า Hint ใหม่
+                InputScore.setText(""); // ล้างช่องกรอก
+                InputScore.setHint("ใส่ตัวเลขทศนิยม่"); // ตั้งค่า Hint เดิม
+
+            } catch (NumberFormatException e) {
+                //ถ้า score ไม่เป็นตัวเลขจะแจ้ง toast
+                Toast.makeText(this, "กรุณากรอกคะแนนเป็นตัวเลข", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            //ถ้า input empty จะ toast
+            Toast.makeText(this, "กรุณากรอกคะแนน", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
 }
